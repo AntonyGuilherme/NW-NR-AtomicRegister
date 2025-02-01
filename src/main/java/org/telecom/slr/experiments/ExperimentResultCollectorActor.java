3371c50373af978b641f0046e26dac2aff0b6c44
Antony Guilherme Costa do Amaral <61386256+AntonyGuilherme@users.noreply.github.com>
2025-02-01 22:05:26 +0100

    Logging configuration

--- src/main/java/org/telecom/slr/experiments/ExperimentResultCollectorActor.java
+++ src/main/java/org/telecom/slr/experiments/ExperimentResultCollectorActor.java
@@ -1,6 +1,8 @@
 package org.telecom.slr.experiments;
 
 import akka.actor.AbstractActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import org.telecom.slr.actor.Actor;
 import org.telecom.slr.actor.messages.ReadIssued;
 import org.telecom.slr.actor.messages.WriteIssued;
@@ -9,13 +11,14 @@ import java.util.List;
 
 public class ExperimentResultCollectorActor extends Actor {
     public static List<ExperimentResultModel> experiments = new LinkedList<>();
+    private LoggingAdapter logger = Logging.getLogger(getContext().getSystem(), this);
 
     public ExperimentResultCollectorActor() {
         run(this::addExperimentWriteIssuedResult).when(message -> message instanceof WriteIssued);
         run(this::addExperimentReadIssuedResult).when(message -> message instanceof ReadIssued);
 
-//        run(this::log).when(message -> message instanceof WriteIssued);
-//        run(this::log).when(message -> message instanceof ReadIssued);
+       run(this::log).when(message -> message instanceof WriteIssued);
+       run(this::log).when(message -> message instanceof ReadIssued);
     }
 
     public static void fromNowOnCollect(int numberOfProcess, int numberOfMessage, int numberOfFaultyProcesses) {
@@ -37,6 +40,6 @@ public class ExperimentResultCollectorActor extends Actor {
         String from = context.sender().path().name();
         String to = context.self().path().name();
 
-        System.out.printf("from %s to %s : %s%n", from, to, message);
+        logger.info(String.format("from %s to %s : %s", from, to, message));
     }
 }
